41a5f6e154b107ec56a90d9d33f324ad5e06ca31
Dmitrii <dtiuterev@rambler.ru>
2022-08-31 13:22:55 +0300

    added Ex23

---
+++ Ex23/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа ввода числа N и вывода таблицы кубов от 1 до N 
+Console.WriteLine("Введите натуральное число, а я выведу список кубов чисел от 1 до этого числа: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Кубы чисел от 1 до {N}");
+if (N > 0)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        Console.WriteLine(Math.Pow(i, 3));
+    }
+}
+else
+{
+    System.Console.WriteLine("Введено некорректное число");
+}
