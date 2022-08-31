9deda09da8a7a44fbb85fb2eee1badb5ed21dddf
Dmitrii <dtiuterev@rambler.ru>
2022-08-31 13:05:42 +0300

    added Ex19

---
+++ Ex19/Program.cs
@@ -0,0 +1,11 @@
+﻿// Проверка палиндромности вводимого пятизначного числа 
+Console.WriteLine("Введите пятизначное число, а я проверю, является ли оно палиндромом: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(Number);
+if (stringNumber.Length == 5)
+    if (stringNumber[0] == stringNumber[4] && stringNumber[1] == stringNumber[3])
+        Console.WriteLine("Число является полиадромом");
+    else
+        Console.WriteLine("Число не является полиадромом");
+else
+    Console.WriteLine("Введено некорректное число");
